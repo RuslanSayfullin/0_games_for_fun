ea678c0768b8b6312c1c2cc7e93303267ca860f5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2020-06-25 18:35:00 +0500

    25/06/20, 1st commit

--- 0_alien_invasion/alien_invasion.py
+++ 0_alien_invasion/alien_invasion.py
@@ -1,26 +1,34 @@
 import sys
-
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
-    # Инициализирует игру и создает объект экрана.
+    # Инициализирует pygame, settings и объект экрана.
     pygame.init()   # инициализирует настройки, необходимые Pygame для нормальной работы.
-    screen = pygame.display.set_mode((1200, 800))   # создает отображаемую область, на которой прорисовываются все графические элементы игры. Аргумент (1200, 800) представляет собой кортеж, определяющий размеры игрового окна.
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))   # создает область, где прорисовываются лементы игры.
     pygame.display.set_caption("Alien Invasion")
 
     # Назначение цвета фона.
     bg_color = (230, 230, 230)
 
+    # Создание корабля.
+    ship = Ship(screen)
+
     # Запуск основного цикла игры.
-    while True:     # цикл, который управляет обновлениями экрана.
+    while True:
         # Отслеживание событий клавиатуры и мыши.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # При каждом проходе цикла перерисовывается экран.
-            screen.fill(bg_color)
+        # При каждом проходе цикла перерисовывается экран.
+        screen.fill(ai_settings.bg_color)
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
+
 run_game()
+
